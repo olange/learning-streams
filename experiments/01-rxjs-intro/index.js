d8546b317e9907f7de741b6d415789dfd2538010
Olivier Lange <olange@petit-atelier.ch>
2019-02-11 21:00:45 +0100

    Adds first example for RxJS (#1)

---
+++ experiments/01-rxjs-intro/index.js
@@ -0,0 +1,16 @@
+const { Observable } = require( "rxjs");
+
+const foo = new Observable(subscriber => {
+  console.log('Hello');
+  subscriber.next(42);
+  subscriber.next(43);
+  setTimeout( () => { subscriber.next(45) }, 1000);
+});
+
+foo.subscribe(x => {
+  console.log(x);
+});
+
+foo.subscribe(y => {
+  console.log(y);
+});
